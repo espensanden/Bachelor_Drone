15a247a1d14386924deab9f0b100a38d60c01c55
espensanden <espen.sanden123@gmail.com>
2024-04-09 17:53:41 +0200

    dada

--- presicion_landing.py
+++ presicion_landing.py
@@ -114,8 +114,8 @@ if now_landing == 0:
     total_time=end_time-start_time
     total_time=abs(int(total_time))
     print("Drone is landing!!")
-    if found_count > 150:
-        vehicle_land == False
+    if found_count > 100:
+        vehicle_land = False
 
     total_count=found_count+notfound_count
     freq_lander=total_count/total_time
