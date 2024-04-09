6fbb7c3538970ea5c8c5da5d896f698838b07b0e
espensanden <espen.sanden123@gmail.com>
2024-04-09 15:24:34 +0200

    changed some stuff

--- aruco_tester.py
+++ aruco_tester.py
@@ -1,22 +1,18 @@
 import cv2
 import cv2.aruco as aruco
 import numpy as np
-from imutils.video import WebcamVideoStream
-import imutils
 
 import time
 import os
 import platform
 import sys
 from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutput
-#############################
 
+#Resolution
 horizontal_res = 640
 vertical_res = 480
 
-
+#Camera initialization
 cv2.startWindowThread()
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (horizontal_res,vertical_res)
@@ -25,17 +21,18 @@ picam2.preview_configuration.align()
 picam2.start()
 
 
-#cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 viewVideo=False
 if len(sys.argv)>1:
     viewVideo=sys.argv[1]
     if viewVideo=='0' or viewVideo=='False' or viewVideo=='false':
         viewVideo=False
-############ARUCO/CV2############
+
+
+#OpenCV stuff
 id_to_find=0
 marker_size=10 #cm
 
-realWorldEfficiency=.7 ##Iterations/second are slower when the drone is flying. This accounts for that
+realWorldEfficiency=.7 ##Since the Iterations/second are slower when the drone is flying the effeciency will be lower when flying.
 
 cameraMatrix = np.array([[774.5585769798772, 0.0, 619.694166336029],
                          [0.0, 772.9641015632712, 352.49790332793935],
@@ -45,7 +42,6 @@ distCoeffs = np.array([-0.3653858593342419, 0.1632243853386151, -0.0026716333098
 
 aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 parameters = cv2.aruco.DetectorParameters_create()
-#############################
 
 seconds=0
 if viewVideo==True:
@@ -61,11 +57,7 @@ counter=float(counter)
 
 start_time=time.time()
 while time.time()-start_time<seconds:
-    #frame = cap.read() #for Threaded webcam
     im = picam2.capture_array()
-#    frame = cv2.resize(frame,(width,height))
-    
-    #frame_np = np.array(im)
     gray_img = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     ids=''
     corners, ids, rejected = aruco.detectMarkers(image=gray_img,dictionary=aruco_dict,parameters=parameters)
@@ -78,8 +70,8 @@ while time.time()-start_time<seconds:
         x="{:.2f}".format(tvec[0])
         y="{:.2f}".format(tvec[1])
         z="{:.2f}".format(tvec[2])
-        #print("FOUND ARUCO!")
-        marker_position="MARKER POSITION: x="+x+" y="+y+" z="+z
+
+        marker_position="Marker position: x="+x+" y="+y+" z="+z
         print(marker_position)
         print("")
         if viewVideo==True:
@@ -89,7 +81,7 @@ while time.time()-start_time<seconds:
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
     else:
-        print("ARUCO "+str(id_to_find)+"NOT FOUND IN FRAME.")
+        print("Aruco nmbr: "+str(id_to_find)+" not found in frame.")
         print("")
     counter=float(counter+1)
 
@@ -97,12 +89,12 @@ if viewVideo==False:
     frequency=realWorldEfficiency*(counter/seconds)
     print("")
     print("")
-    print("---------------------------")
-    print("Loop iterations per second:")
+    print("")
+    print("Iterations per second:")
     print(frequency)
-    print("---------------------------")
+    print("")
 
-    print("Performance Diagnosis:")
+    print("Performance:")
     if frequency>10:
         print("Performance is more than enough for great precision landing.")
     elif frequency>5:
@@ -110,6 +102,6 @@ if viewVideo==False:
         print("This resolution likely maximizes the detection altitude of the marker.")
     else:
         print("Performance likely not good enough for precision landing.")
-        print("MAKE SURE YOU HAVE A HEAT SINK ON YOUR PI!!!")
+        print("Check if the Pi is too hot")
     print("---------------------------")
 cv2.destroyAllWindows()
