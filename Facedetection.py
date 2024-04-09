99d79f5db8b05af564710523da3b1b63c8b492f2
espensanden <espen.sanden123@gmail.com>
2024-04-09 13:52:42 +0200

    change

--- Facedetection.py
+++ Facedetection.py
@@ -17,10 +17,7 @@ while True:
     im = picam2.capture_array()
 
     grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(grey, 1.1, 5)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0))
+    
 
     cv2.imshow("Camera", im)
     cv2.waitKey(1)
