d4aa7caf2cf65ec7eb06dc4c68321ffb39183e3a
Espen Sanden <espen.sanden123@gmail.com>
2024-04-10 10:01:25 +0200

    yep

--- pres_land_w_feed.py
+++ pres_land_w_feed.py
@@ -5,20 +5,15 @@ from picamera2 import Picamera2
 import time
 
 
-
 first_run = 0
 start_time = 0
-start_time=0
-end_time=0
+end_time = 0
 
-
-found_count=0
-notfound_count=0
-
-
+found_count = 0
+notfound_count = 0
 
 id_to_find = 0
-aruco_marker_size = 10 #in cm
+aruco_marker_size = 10  # in cm
 
 now_landing = 0
 
@@ -31,40 +26,40 @@ distCoeffs = np.array([-0.3653858593342419, 0.1632243853386151, -0.0026716333098
 aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
 parameters = cv2.aruco.DetectorParameters_create()
 
-#Camera
+# Camera
 horizontal_res = 640
 vertical_res = 480
 picam2 = Picamera2()
-picam2.preview_configuration.main.size = (horizontal_res,vertical_res)
+picam2.preview_configuration.main.size = (horizontal_res, vertical_res)
 picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
 
-#Camera FOV
-horizontal_fov = 62.2 * (math.pi / 180 )
+
+# Camera FOV
+horizontal_fov = 62.2 * (math.pi / 180)
 vertical_fov = 48.8 * (math.pi / 180)
 
 vehicle_land = True
 
 
 def landing_drone():
-    #while True:
     global first_run, notfound_count, found_count, aruco_marker_size, start_time, vehicle_land
-    if first_run==0:
+    if first_run == 0:
         print("First run of lander!!")
-        first_run=1
-        start_time=time.time()
+        first_run = 1
+        start_time = time.time()
     im = picam2.capture_array()
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     ids = ''
     corners, ids, _ = cv2.aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-    
+
     try:
         if ids is not None and ids[0] == id_to_find:
-            ret  = cv2.aruco.estimatePoseSingleMarkers(corners, aruco_marker_size, cameraMatrix, distCoeffs)
+            ret = cv2.aruco.estimatePoseSingleMarkers(corners, aruco_marker_size, cameraMatrix, distCoeffs)
             (rvec, tvec) = (ret[0][0, 0, :], ret[1][0, 0, :])
-                    
+
             x = '{:.2f}'.format(tvec[0])
             y = '{:.2f}'.format(tvec[1])
             z = '{:.2f}'.format(tvec[2])
@@ -72,58 +67,52 @@ def landing_drone():
             y_sum = 0
             x_sum = 0
 
-            x_sum = corners[0][0][0][0]+ corners[0][0][1][0]+ corners[0][0][2][0]+ corners[0][0][3][0]
-            y_sum = corners[0][0][0][1]+ corners[0][0][1][1]+ corners[0][0][2][1]+ corners[0][0][3][1]
+            x_sum = corners[0][0][0][0] + corners[0][0][1][0] + corners[0][0][2][0] + corners[0][0][3][0]
+            y_sum = corners[0][0][0][1] + corners[0][0][1][1] + corners[0][0][2][1] + corners[0][0][3][1]
 
+            x_avg = x_sum * .25
+            y_avg = y_sum * .25
 
-            x_avg = x_sum*.25
-            y_avg = y_sum*.25
+            x_ang = (x_avg - horizontal_res * .5) * (horizontal_fov / horizontal_res)
+            y_ang = (y_avg - vertical_res * .5) * (vertical_fov / vertical_res)
 
-            x_ang = (x_avg - horizontal_res*.5)*(horizontal_fov/horizontal_res)
-            y_ang = (y_avg - vertical_res*.5)*(vertical_fov/vertical_res)
-
-
-            
-
-            print("x centre pixel: "+str(x_avg)+" y centre pixel: "+str(y_avg))
-            print("Marker position: x = "+x+" y = "+y+" z = "+z)
-            found_count=found_count+1
+            print("x centre pixel: " + str(x_avg) + " y centre pixel: " + str(y_avg))
+            print("Marker position: x = " + x + " y = " + y + " z = " + z)
+            found_count = found_count + 1
             print("")
 
-            if found_count >= 100:
+            if found_count >= 200:
                 vehicle_land = False
 
         else:
-            notfound_count=notfound_count+1
+            notfound_count = notfound_count + 1
 
     except Exception as e:
-        print('Target likely not found. Error: '+str(e))
-        notfound_count=notfound_count+1
+        print('Target likely not found. Error: ' + str(e))
+        notfound_count = notfound_count + 1
+
     cv2.imshow('Frame', im)  # Display the frame
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        #break
+        vehicle_land = False
 
-        im.release()  # Release the capture object
-    cv2.destroyAllWindows()  # Close all OpenCV windows
-
+    im.release()  # Release the capture object
 
 
-
-if now_landing == 0:
-    while vehicle_land == True:
+if __name__ == "__main__":
+    while vehicle_land:
         landing_drone()
-    end_time=time.time()
-    total_time=end_time-start_time
-    total_time=abs(int(total_time))
+    end_time = time.time()
+    total_time = end_time - start_time
+    total_time = abs(int(total_time))
     print("Drone is landing!!")
 
-total_count=found_count+notfound_count
-freq_lander=total_count/total_time
-print("Total iterations: "+str(total_count))
-print("Total seconds: "+str(total_time))
-print("*******************")
-print("Landing_drone function had frequency of: "+str(freq_lander))
-print("*******************")
-print("Vehicle has landed")
-print("*******************")
+    total_count = found_count + notfound_count
+    freq_lander = total_count / total_time
+    print("Total iterations: " + str(total_count))
+    print("Total seconds: " + str(total_time))
+    print("*******************")
+    print("Landing_drone function had frequency of: " + str(freq_lander))
+    print("*******************")
+    print("Vehicle has landed")
+    print("*******************")
