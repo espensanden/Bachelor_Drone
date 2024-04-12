9b7f8f019c3f6503bd45f103aeaab5ef4118de63
espensanden <espen.sanden123@gmail.com>
2024-04-12 13:52:56 +0200

    Changed target alltitude to 1m

--- takeoff_script.py
+++ takeoff_script.py
@@ -7,7 +7,7 @@ from dronekit import connect, VehicleMode,LocationGlobal,LocationGlobalRelative
 from pymavlink import mavutil
 #############################
 
-targetAltitude=1.5
+targetAltitude=1
 manualArm=False
 ############DRONEKIT#################
 vehicle = connect('/dev/ttyAMA0',baud=57600,wait_ready=True)
