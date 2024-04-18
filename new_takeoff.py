f64c420e587110f3185da3535902265c9b039064
espensanden <espen.sanden123@gmail.com>
2024-04-18 14:31:38 +0200

    new takeoff

--- new_takeoff.py
+++ new_takeoff.py
@@ -5,58 +5,44 @@ import sys
 
 from dronekit import connect, VehicleMode,LocationGlobal,LocationGlobalRelative
 from pymavlink import mavutil
-#############################
 
-targetAltitude=1
-manualArm=False
-############DRONEKIT#################
+
+
+# Connect to the Vehicle (in this case a simulator running the same computer)
 vehicle = connect('/dev/ttyAMA0',baud=57600,wait_ready=True)
 
-#Select /dev/ttyAMA0 for UART. /dev/ttyACM0 for USB
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
 
-#########FUNCTIONS###########
-def arm_and_takeoff(targetHeight):
-	while vehicle.is_armable!=True:
-		print("Waiting for vehicle to become armable.")
-		time.sleep(1)
-	print("Vehicle is now armable")
-    
-	vehicle.mode = VehicleMode("GUIDED")
-            
-	while vehicle.mode!='GUIDED':
-		print("Waiting for drone to enter GUIDED flight mode")
-		time.sleep(1)
-	print("Vehicle now in GUIDED MODE. Have fun!!")
-
-    if manualArm==False:
-        vehicle.armed = True
-        while vehicle.armed==False:
-            print("Waiting for vehicle to become armed.")
-            time.sleep(1)
-    else:
-        if vehicle.armed == False:
-            print("Exiting script. manualArm set to True but vehicle not armed.")
-            print("Set manualArm to True if desiring script to arm the drone.")
-            return None
-    print("Look out! Props are spinning!!")
-            
-	vehicle.simple_takeoff(targetHeight) ##meters
-
-	while True:
-		print("Current Altitude: %d"%vehicle.location.global_relative_frame.alt)
-		if vehicle.location.global_relative_frame.alt>=.95*targetHeight:
-			break
-		time.sleep(1)
-	print("Target altitude reached!!")
-
-	return None
-
-############MAIN###############
-
-arm_and_takeoff(targetAltitude)
-vehicle.mode = VehicleMode("LAND")
-
-while vehicle.mode!='LAND':
+    print("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print(" Waiting for vehicle to initialise...")
         time.sleep(1)
-        print("Waiting for drone to land")
-print("Drone in land mode. Exiting script.")
+
+    print("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode    = VehicleMode("GUIDED")
+    vehicle.armed   = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print(" Waiting for arming...")
+        time.sleep(1)
+
+    print("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            print("Reached target altitude")
+            break
+        time.sleep(1)
+
+arm_and_takeoff(3)
