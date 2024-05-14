147f4cb357043083d6db06942d870b356ed7c033
espensanden <espen.sanden123@gmail.com>
2024-05-14 09:35:10 +0200

    changed altitude

--- takeoff_presicionland.py
+++ takeoff_presicionland.py
@@ -8,7 +8,7 @@ from pymavlink import mavutil
 
 
 #Variables
-takeoff_height = 2 #meter
+takeoff_height = 3 #meter
 velocity = 0.5
 
 first_run = 0
@@ -20,7 +20,8 @@ end_time=0
 found_count=0
 notfound_count=0
 
-targetAltitude = 2
+#targetaltitude
+targetAltitude = 3
 
 id_to_find = 0
 aruco_marker_size = 10 #in cm
